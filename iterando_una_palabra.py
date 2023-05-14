6d4052f90edb55db64a15a12fd304eb5849f8ad2
Carolina Lara <lalarita@hotmail.com>
2023-05-14 19:15:16 -0400

    Iniciando Drilling

---
+++ iterando_una_palabra.py
@@ -0,0 +1,27 @@
+"""
+Requerimos eliminar todas las vocales de la palabra “paralelepípedo”, e imprimir en pantalla 
+las consonantes restantes y su posición dentro de dicha palabra.
+"""
+palabra= "paralelepípedo"
+
+#Se realiza un for para que recorra los caracteres de la palabra dada"
+
+for l in palabra:
+    if l == "a" or l == "e" or l=="í" or l =="o":
+     continue
+    print(l)
+   
+        
+
+    
+       
+        
+        
+    
+    
+    
+    
+   
+
+    
+    
